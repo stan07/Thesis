33b5810ce27cd4a69804340a76a43c857a2e83c4
Stan <stanleyyu07@gmail.com>
2013-06-21 16:40:37 +0800

    Thesis Project Committed 2
    Server project included...

---
+++ Server/src/com/example/server/Constants.java
@@ -0,0 +1,11 @@
+package com.example.server;
+
+public class Constants {
+	
+	public static final int 
+		SERVERID = 1,
+		TAXIID = 2,
+		CLIENTID = 3,
+		PORT = 8888;
+	
+}
---
+++ Server/src/com/example/server/Server.java
@@ -0,0 +1,138 @@
+package com.example.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable{
+	
+	private Socket clientSocket;
+	private static String clientCoordinates = "", taxiCoordinates = "";
+	
+	public Server(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+		
+	public static void main(String[] args){
+
+		ServerSocket serverSocket = null;
+
+		//attempt to create a new server for data transfers
+		try {
+			serverSocket = new ServerSocket(Constants.PORT);
+			System.out.println("LISTENING TO PORT: " + Constants.PORT);
+		} catch (IOException e) {
+			System.out.println("FAILED TO LISTEN TO PORT...");
+			e.printStackTrace();
+		}
+	  
+		//process continues if the server has been established then generate a server-client connection
+		while(true)
+		{
+			try {
+				Socket socket = serverSocket.accept();
+				new Thread(new Server(socket)).start();
+			} catch (IOException e) {
+				System.out.println("Failed to connect to client...");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void run() {
+		
+		int ID = 0;
+		DataInputStream input = null;
+		DataOutputStream output = null;
+		
+		try 
+		{
+			String clientIP = clientSocket.getInetAddress().toString();
+			System.out.println("Connected to client: " + clientIP);
+			
+			input = new DataInputStream(clientSocket.getInputStream());
+			output = new DataOutputStream(clientSocket.getOutputStream());
+
+			try{
+				ID = Integer.parseInt(input.readUTF());
+				System.out.println("ID: " + ID);
+			} catch(NumberFormatException e) {
+				output.writeUTF("ID not found!");
+				e.printStackTrace();
+			}
+		
+			if(ID == Constants.SERVERID)
+			{
+				output.writeUTF(taxiCoordinates);
+				String serverMsg = input.readUTF();
+				System.out.println("Server Message: " + serverMsg);
+			}
+			
+			else if(ID == Constants.TAXIID)
+			{	
+				String taxiAction = input.readUTF();
+				
+				if(taxiAction.equals("LISTEN"))
+				{	
+					if(!clientCoordinates.equals(""))
+					{
+						output.writeUTF(clientCoordinates);
+						clientCoordinates = "";
+					}	
+					
+					else
+						output.writeUTF("NULL");
+				}
+				
+				else
+				{
+					taxiCoordinates = input.readUTF();
+					System.out.println("Taxi Coordinates: " + taxiCoordinates);
+				}
+			}
+			
+			else
+			{
+				clientCoordinates = input.readUTF();
+				System.out.println("Client Coordinates: " + clientCoordinates);
+				output.writeUTF("Coordinates received by server!");
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		finally
+		{
+			if(clientSocket!= null)
+			{
+				try {
+					clientSocket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if(input!= null)
+			{
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+			
+			if(output!= null)
+			{
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
